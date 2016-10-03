1a7eb7ebfe0d5f02c0e8b3e3f99c3eab345c0404
Danilo Silva <badkill82@gmail.com>
2016-10-03 23:32:16 +0200

    completed bob exercism

---
+++ bob/src/lib.rs
@@ -0,0 +1,18 @@
+extern crate regex;
+use regex::Regex;
+
+pub fn reply(question: &str) -> &str {
+    let constraints = vec![
+        ("^$", "Fine. Be that way!"),
+        ("^[^a-z]+$", "Whoa, chill out!"),
+        ("^.*\\?$", "Sure."),
+    ];
+
+    for pair in constraints.into_iter() {
+        if Regex::new(pair.0).unwrap().is_match(question) {
+            return pair.1
+        }
+    }
+
+    "Whatever."
+}
