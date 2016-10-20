9111bf7fec8440beb55ad3d8c796c073aace8617
SAMPLE <SAMPLE>
2016-10-20 23:47:52 +0200

    refacorized sublist

--- sublist/src/lib.rs
+++ sublist/src/lib.rs
@@ -1,4 +1,3 @@
-use std::cmp::Ordering;
 use std::fmt::Debug;
 
 #[derive(Debug, PartialEq)]
@@ -10,47 +9,19 @@ pub enum Comparison {
 }
 
 pub fn sublist<T: PartialEq + Debug + Sized>(v1: &[T], v2: &[T]) -> Comparison {
-    match v1.len().cmp(&v2.len()) {
-        Ordering::Equal => compare(v1, v2),
-        Ordering::Less => {
-            if v1.is_empty() {
-                return Comparison::Sublist;
-            }
-
-            let firstOfV1 = &v1[0];
-            if v2.iter().enumerate().any(|(i, elementOfV2)| {
-                if firstOfV1 == elementOfV2 {
-                    match compare(v1, &v2[i..]) {
-                        Comparison::Equal => return true,
-                        _ => return false,
-                    }
-                }
-
-                false
-            }) {
-                return Comparison::Sublist;
-            }
-
-            Comparison::Unequal
-        }
-        Ordering::Greater => {
-            if Comparison::Sublist == sublist(v2, v1) {
-                return Comparison::Superlist;
-            }
-
-            Comparison::Unequal
-        }
+    if v1 == v2 {
+        Comparison::Equal
+    } else if is_sublist(v1, v2) {
+        Comparison::Sublist
+    } else if is_sublist(v2, v1) {
+        Comparison::Superlist
+    } else {
+        Comparison::Unequal
     }
 }
 
-fn compare<T: PartialEq + Debug + Sized>(v1: &[T], v2: &[T]) -> Comparison {
-    if v1.len() > v2.len() {
-        return Comparison::Unequal;
-    }
-
-    if v1.iter().zip(v2.iter()).all(|(x, y)| x == y) {
-        return Comparison::Equal;
-    }
-
-    Comparison::Unequal
+fn is_sublist<T: PartialEq + Debug + Sized>(v1: &[T], v2: &[T]) -> bool {
+    v1.is_empty() || v2.windows(v1.len()).any(|window| {
+        v1.iter().zip(window).all(|(x, y)| x == y)
+    })
 }
