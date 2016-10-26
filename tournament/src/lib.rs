49ff9f4c0335871a3c0dd9d46e5332619fb7c30d
SAMPLE <SAMPLE>
2016-10-26 22:58:23 +0200

    refactorized tournament

--- tournament/src/lib.rs
+++ tournament/src/lib.rs
@@ -1,6 +1,8 @@
-use std::collections::HashMap;
-use std::str::FromStr;
 use std::cmp::Ordering;
+use std::collections::HashMap;
+use std::fmt::Display;
+use std::str::FromStr;
+
 
 #[derive(Debug)]
 struct InvalidValue;
@@ -26,14 +28,14 @@ impl FromStr for Outcome {
 
 #[derive(Debug)]
 struct PlayedMatch {
-    home: String,
-    visitor: String,
+    home_name: String,
+    visitor_name: String,
     outcome: Outcome,
 }
 
 impl FromStr for PlayedMatch {
     type Err = InvalidValue;
-    fn from_str(s: & str) -> Result<Self, Self::Err> {
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
         let splitted: Vec<&str> = s.split(';').collect();
         if 3 != splitted.len() {
             return Err(InvalidValue);
@@ -41,93 +43,80 @@ impl FromStr for PlayedMatch {
 
         let outcome = try!(splitted[2].parse());
         Ok(PlayedMatch {
-            home: splitted[0].to_string(),
-            visitor: splitted[1].to_string(),
-            outcome: outcome
+            home_name: splitted[0].to_string(),
+            visitor_name: splitted[1].to_string(),
+            outcome: outcome,
         })
     }
 }
 
 impl PlayedMatch {
-    pub fn process(&self, league: &mut HashMap<String, Team>) -> bool {
-        {
-            let ref mut home_team = league.entry(self.home.clone())
-                .or_insert(Team::new(self.home.clone()));
-            match self.outcome {
-                Outcome::Win => {
-                    home_team.win();
-                }
-                Outcome::Loss => {
-                    home_team.lost();
-                }
-                Outcome::Draw => {
-                    home_team.draw();
-                }
+    pub fn process(&self, league: &mut HashMap<String, Team>) {
+        match self.outcome {
+            Outcome::Win => {
+                Team::from_league(self.home_name.clone(), league).win();
+                Team::from_league(self.visitor_name.clone(), league).loss();
+            }
+            Outcome::Loss => {
+                Team::from_league(self.home_name.clone(), league).loss();
+                Team::from_league(self.visitor_name.clone(), league).win();
+            }
+            Outcome::Draw => {
+                Team::from_league(self.home_name.clone(), league).draw();
+                Team::from_league(self.visitor_name.clone(), league).draw();
             }
         }
-        {
-            let ref mut visitor_team = league.entry(self.visitor.clone())
-                .or_insert(Team::new(self.visitor.clone()));
-            match self.outcome {
-                Outcome::Win => {
-                    (*visitor_team).lost();
-                }
-                Outcome::Loss => {
-                    (*visitor_team).win();
-                }
-                Outcome::Draw => {
-                    (*visitor_team).draw();
-                }
-            }
-        }
-
-        true
     }
 }
 
 #[derive(Default, Debug, Eq, PartialEq, PartialOrd)]
 struct Team {
     name: String,
-    mp: u8,
-    w: u8,
-    d: u8,
-    l: u8,
-    point: u8,
+    match_played: u8,
+    wins: u8,
+    draws: u8,
+    losses: u8,
+    points: u8,
 }
 
 impl Team {
+    pub fn from_league(name: String, league: &mut HashMap<String, Team>) -> &mut Self {
+        league.entry(name.clone())
+            .or_insert(Team::new(name.clone()))
+    }
+
     pub fn new(name: String) -> Self {
         Team {
             name: name,
-            mp: 0,
-            w: 0,
-            d: 0,
-            l: 0,
-            point: 0,
+            match_played: 0,
+            wins: 0,
+            draws: 0,
+            losses: 0,
+            points: 0,
         }
     }
 
     pub fn win(&mut self) {
-        self.mp += 1;
-        self.w += 1;
-        self.point += 3;
+        self.match_played += 1;
+        self.wins += 1;
+        self.points += 3;
     }
 
-    pub fn lost(&mut self) {
-        self.mp += 1;
-        self.l += 1;
+    pub fn loss(&mut self) {
+        self.match_played += 1;
+        self.losses += 1;
     }
 
     pub fn draw(&mut self) {
-        self.mp += 1;
-        self.d += 1;
-        self.point += 1;
+        self.match_played += 1;
+        self.draws += 1;
+        self.points += 1;
     }
 }
 
 impl Ord for Team {
     fn cmp(&self, other: &Team) -> Ordering {
-        match other.point.cmp(&self.point) {
+        match other.points.cmp(&self.points) {
             Ordering::Equal => self.name.cmp(&other.name),
             order => order,
         }
@@ -143,21 +132,42 @@ pub fn tally(played_matches: &String) -> String {
         }
     }
 
-    let mut league: Vec<_> = league.values().collect();
+    Ranking::new(league.values().collect()).display()
+}
 
-    league.sort();
+struct Ranking<'a> {
+    teams: Vec<&'a Team>,
+}
 
-    let mut s = format!("{:31}| MP |  W |  D |  L |  P", "Team");
-    for team in league {
-        let ref partial = format!("\n{:31}|  {} |  {} |  {} |  {} |  {}",
-                       team.name,
-                       team.mp,
-                       team.w,
-                       team.d,
-                       team.l,
-                       team.point);
-        s.push_str(partial);
+impl<'a> Ranking<'a> {
+    pub fn new(teams: Vec<&'a Team>) -> Self {
+        Ranking { teams: teams }
     }
 
-    s
+    pub fn display(&mut self) -> String {
+        self.teams.sort();
+
+        let mut output = vec![self.format_row(&["Team", "MP", "W", "D", "L", "P"])];
+
+        output.append(&mut self.teams
+            .iter()
+            .map(|ref team| self.format_team(team))
+            .collect::<Vec<String>>());
+
+        output.join("\n")
+    }
+
+    fn format_team(&self, team: &Team) -> String {
+        self.format_row(&[team.name.to_string(),
+                           team.match_played.to_string(),
+                           team.wins.to_string(),
+                           team.draws.to_string(),
+                           team.losses.to_string(),
+                           team.points.to_string()])
+    }
+
+    fn format_row<T: Display>(&self, data: &[T]) -> String {
+        format!("{0:<30} | {1:>2} | {2:>2} | {3:>2} | {4:>2} | {5:>2}",
+                data[0], data[1], data[2], data[3], data[4], data[5])
+    }
 }
